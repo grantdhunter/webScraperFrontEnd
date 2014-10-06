e5282d587f35d54992919ca2468190c596f3973a
grantdhunter <gr@nt-hunter.ca>
2014-10-05 20:34:29 -0600

    Revert "finishing touches"
    This reverts commit 35bf14eaccda47a20a4e8616ca1ac590ff0af503.

--- public/system/controllers/homeCtrl.js
+++ public/system/controllers/homeCtrl.js
@@ -11,19 +11,4 @@ function homeCtrl($scope, $routeParams, scrapeData) {
         });
     };
 
-    $scope.codeExample = "function (elem) {\n\
-    var $ = cheerio.load(elem);\n\
-    var deferred = Q.defer();\n\
-    \n\
-    var link = $(elem).find('a').attr('href');\n\
-    var content = $(elem).text().replace(/\s{2,9999}/g, ' ').trim();\n\
-    var temp = {link: link,\n\
-                content: content\n\
-                };\n\
-    if (temp.link) {\n\
-        deferred.resolve(temp)\n\
-    } else {\n\
-        deferred.resolve(null)};\n\
-    return deferred.promise;\n\
-};"
 }
