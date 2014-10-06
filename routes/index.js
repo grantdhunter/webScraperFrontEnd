b804c6368789124f7fa65d90e657bc9d226e7c74
grantdhunter <gr@nt-hunter.ca>
2014-10-05 20:35:45 -0600

    Revert "Revert "finishing touches""
    This reverts commit e5282d587f35d54992919ca2468190c596f3973a.

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,33 @@ var express = require('express');
 var router = express.Router();
 var scraper = require('webScraper');
 
+var cheerio = require('cheerio');
+var request = require('request');
+var Q = require('q');
+
 router.get('/partials/:name', function (req, res) {
     res.render('partials/' + req.params.name);
 });
 
 router.get('/scrape', function (req, res) {
-    console.log(req.query.url);
-    console.log(req.query.class);
+
+    eval("var crawlModule = "+req.query.function);
+    
     scraper.crawlForData(req.query.url,
         req.query.class,
         function (data) {
-            console.log(data);
-        //don't know why but angular throws error if I send an array
-            res.send({data:data});
-        });
+            //don't know why but angular throws error if I send an array
+            res.send({
+                data: data
+            });
+        },
+        crawlModule
+    )
 });
 
 router.get('*', function (req, res) {
     res.render('index');
 });
 module.exports = router;
+
+//eval("var c = function (elem) {var $ = cheerio.load(elem);var deferred = Q.defer();var link = $(elem).find('a').attr('href');var content = $(elem).text().replace(/\s{2,9999}/g, ' ').trim();var temp = {link: link,content: content};if (temp.link) {deferred.resolve(temp)} else {deferred.resolve(null)};return deferred.promise;};");
